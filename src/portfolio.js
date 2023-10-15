8cef125135f46dfe5f378b788d3352e82075fc98
Potluri Sri Harshitha <potlurisriharshitha@gmail.com>
2023-10-15 12:08:17 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,60 @@
+const header = {
+    title: 'Hello I am Sri Harshitha!',
+  }
+  
+  const about = {
+    name: 'Sri Harshitha',
+    role: 'Computer Science Engineer',
+    description:
+      'Currently pursuing my B.tech 3rd year at VIT AP University.',
+    resume: 'https://example.com',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/potluri-sri-harshitha-12b182249/',
+      github: 'https://github.com/sharshithapotluri',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'Project 1',
+      description:
+        'Research Paper on PCOD and PCOS pre-hand Detection',
+    },
+    {
+      name: 'Project 2',
+      description:
+        'Reasearch Paper on Agriculture and Biodiversity-Protecting farmers  seeds.',
+      stack: ['SASS', 'TypeScript', 'React'],
+    
+    },
+    {
+        name: 'Project 3',
+        description:
+          'Soil Moist Detection',
+      },
+      {
+        name: 'Project 4',
+        description:
+          'AcciTrack',
+      
+      },
+  ]
+  
+  const skills = [
+    
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Git',
+    'Sql',
+    'Java',
+    'Python'
+  ]
+  
+  const contact = {
+    email: 'potlurisriharshitha@mail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
